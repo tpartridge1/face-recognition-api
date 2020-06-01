c1278473deb942345f7221c40c9579c471f0abe7
tpartridge1 <tammy.partridge7@gmail.com>
2020-05-31 20:44:29 -0400

    adding files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./Controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : '',
     user : 'postgres',
     password : 'KUsadist50!!',
     database : 'smart-brain'
