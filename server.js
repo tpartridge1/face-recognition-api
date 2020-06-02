9c253cc4f7c7d34f5c0613dd85925e918e927bb7
tpartridge1 <tammy.partridge7@gmail.com>
2020-06-02 13:13:53 -0400

    changed db

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
 });
 
 const app = express();
