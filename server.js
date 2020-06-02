49c9939e8fb5bea1e0c951a60a3df6b0d5a231a8
tpartridge1 <tammy.partridge7@gmail.com>
2020-06-02 14:26:36 -0400

    updating files

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
-//process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 const db = knex({
     client: 'pg',
